558fa6565faeec2e564b08277eaf17fd19fc72e6
NiteshSindhu <niteshsindhu1435@gmail.com>
2022-07-24 15:18:59 +0530

    addfile

--- Scripts/car.js
+++ Scripts/car.js
@@ -1,8 +1,54 @@
-// export {navbar,compfirst,complast,footer,footerlast};
+let carsdata=()=>{
+let xy=document.getElementById("card");
+xy.innnerHtml=null;
 
-import {navbar,compfirst,complast,footer,footerlast} from "../Components/navbar.js";
-document.getElementById("navbar").innerHTML=navbar();
-document.getElementById("compfirst").innerHTML=compfirst();
-document.getElementById("complast").innerHTML=complast();
-document.getElementById("footer").innerHTML=footer();
-document.getElementById("footer-last").innerHTML=footerlast();
+    let div1=document.createElement("div");
+    div1.classList.add("div1");
+    let div2=document.createElement("div");
+    div2.classList.add("div2");
+
+    let input1=document.createElement("input");
+    input1.type="text";
+    input1.placeholder="Pick-up";
+    input1.classList.add("form-control");
+    let input2=document.createElement("input");
+    input2.type="text";
+    input2.placeholder="Same Pick-up";
+    input2.classList.add("form-control");
+    div1.append(input1,input2);
+
+    let input5=document.createElement("input");
+    input5.placeholder="Pick-up date";
+    input5.classList.add("form-control");
+    input5.classList.add("select-date");
+    input5.setAttribute("onfocus","(this.type='date')");
+    let input6=document.createElement("input");
+    input6.setAttribute("onfocus","(this.type='date')");
+    input6.placeholder="Drop-off date";
+    input6.classList.add("select-date");
+
+    input6.classList.add("form-control");
+    let input3=document.createElement("input");
+    input3.setAttribute("onfocus","(this.type='time')");
+    input3.placeholder="Pickup time";
+    input3.classList.add("form-control");
+    input3.classList.add("select-date");
+    input3.classList.add("select-time");
+    let input4=document.createElement("input");
+    input4.setAttribute("onfocus","(this.type='time')");
+    input4.placeholder="Dropoff Time";
+    input4.classList.add("form-control");
+    input4.classList.add("select-time");
+    input4.classList.add("select-date");
+
+
+    div2.append(input5,input6,input3,input4);
+
+
+
+
+    xy.append(div1,div2);
+
+    
+
+};
