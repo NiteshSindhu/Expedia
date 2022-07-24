846aef7c5464a123e7fe8a262b2d2d31a650d5e1
ravikr2 <batmanxjoker2@gmail.com>
2022-07-24 11:42:16 +0530

    final

---
+++ Components/image.js
@@ -0,0 +1,21 @@
+function imgbanner(){
+    return `<div id="absolute">
+    <div id="image"></div>
+    <div id="text">
+      <h1>Save instantly with Expedia Rewards</h1>
+      <h4>
+        You can enjoy access to perks like Member Prices, saving you 10% or
+        more on select hotels. Terms may apply.
+      </h4>
+      <button>See Member Prices</button>
+    </div>
+  </div>
+</div>
+<div id="image2">
+<div>
+    <h4>Plane ahead and save</h4>
+    <h5>Book 60days in advance for 20% off select days</h5>
+ </div>`
+}
+
+export default imgbanner
